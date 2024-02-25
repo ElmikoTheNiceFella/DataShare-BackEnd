07a3765910befef8a3eed075d6df27f8787ef94f
ElmikoTheNiceFella <akhadkhou@gmail.com>
2024-02-25 13:19:30 +0300

    Added GET POST placeholders

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send("Hello World")
+app.get('/:formID', (req, res) => {
+  // Get the form with the form ID form the db
+  res.send(`Form ID: ${req.params.formID}`)
+})
+
+app.post('/', (req, res) => {
+  // Save the form with the form ID to the db
+  res.send(`Form ID: ${req.params.formID}`)
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('Server is running...'))
