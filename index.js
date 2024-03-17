9df7070a20de63c393e72b3d6b65fd7054028b6d
ElmikoTheNiceFella <akhadkhou@gmail.com>
2024-03-17 11:03:10 +0300

    Added the necessary middlewares for the post request to work

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const bp = require('body-parser')
+
+app.use(bp.json())
+
+app.use(require('cors')())
 
 app.get('/:formID', (req, res) => {
   // Get the form with the form ID form the db
@@ -9,8 +14,8 @@ app.get('/:formID', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  // Save the form with the form ID to the db
-  res.send(`Form ID: ${req.params.formID}`)
+  console.log(req.body)
+  res.send(`Form ID: ${req.body}`)
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('Server is running...'))
